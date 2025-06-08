9b270c920e0e084046ff420992aa94a7828fbf7f
Tristen Arias <btarias@uw.edu>
2025-06-08 14:32:02 -0700

    Update science.py

--- science.py
+++ science.py
@@ -19,27 +19,7 @@ def reduce_science_frame(
     median_dark_filename,
     reduced_science_filename="reduced_science.fits",
 ):
-    """This function must:
-
-    - Accept a science frame filename as science_filename.
-    - Accept a median bias frame filename as median_bias_filename (the one you created
-      using create_median_bias).
-    - Accept a median flat frame filename as median_flat_filename (the one you created
-      using create_median_flat).
-    - Accept a median dark frame filename as median_dark_filename (the one you created
-      using create_median_dark).
-    - Read all files.
-    - Subtract the bias frame from the science frame.
-    - Subtract the dark frame from the science frame. Remember to multiply the
-      dark frame by the exposure time of the science frame. The exposure time can
-      be found in the header of the FITS file.
-    - Correct the science frame using the flat frame.
-    - Optionally, remove cosmic rays.
-    - Save the resulting reduced science frame to a FITS file with the filename
-      reduced_science_filename.
-    - Return the reduced science frame as a 2D numpy array.
-
-    """
+   
     # Gets the science fits data from science_filename
     science_frame = (fits.getdata(science_filename)).astype('float32')
 
