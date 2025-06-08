735b5e62aac1dd3568386671f5fbf1df2d87d033
Tristen Arias <btarias@uw.edu>
2025-06-08 14:30:48 -0700

    Update darks.py

--- darks.py
+++ darks.py
@@ -10,22 +10,6 @@ from astropy.visualization import ImageNormalize, LinearStretch, ZScaleInterval
 from astropy.stats import sigma_clip
 
 def create_median_dark(dark_list, bias_filename, median_dark_filename):
-    """This function must:
-
-    - Accept a list of dark file paths to combine as dark_list.
-    - Accept a median bias frame filename as bias_filename (the one you created using
-      create_median_bias).
-    - Read all the images in dark_list and create a list of 2D numpy arrays.
-    - Read the bias frame.
-    - Subtract the bias frame from each dark image.
-    - Divide each dark image by its exposure time so that you get the dark current
-      per second. The exposure time can be found in the header of the FITS file.
-    - Use a sigma clipping algorithm to combine all the bias-corrected dark frames
-      using the median and removing outliers outside 3-sigma for each pixel.
-    - Save the resulting dark frame to a FITS file with the name median_dark_filename.
-    - Return the median dark frame as a 2D numpy array.
-
-    """
 
     # Reads the dark image filepaths from dark_list
     dark_images = []
